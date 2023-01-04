508df369802342f2c7403667ee525b85e5eb92ca
Alex <alexandre@predictalab.com>
2023-01-04 21:13:05 +0100

    nvim config

---
+++ lua/alex/core/keymaps.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general keymaps
+keymap.set("i", "jk", "<ESC>") -- return to normal mode when we are in insert mode with "jk"
+
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- undo the selection when search to a specific word
+
+keymap.set("n", "x", '"_x') -- not copy into the clipboard when delete a word
+
+keymap.set("n", "<leader>+", "<C-a>") -- add one to a number
+keymap.set("n", "<leader>-", "<C-x>") -- -1 to a number
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
